b7bd455478371052f158c526feeb3bee5e86f923
Artem Pokutnii <artempokutnij@gmail.com>
2016-06-20 14:52:11 +0300

    change body color, remove sidebar

--- javascripts/views/stories/list.jsx
+++ javascripts/views/stories/list.jsx
@@ -8,14 +8,6 @@ export default class StoryList extends Component {
   render() {
     return (
       <Row>
-        <Col sm={3} md={2} className='sidebar'>
-          <ul className="nav nav-sidebar">
-            <li><a href>Nav item</a></li>
-          </ul>
-          <ul className="nav nav-sidebar">
-            <li><a href>Nav item again</a></li>
-          </ul>
-        </Col>
         <Col sm={9} smOffset={3} md={10} mdOffset={2} className='main'>
           <h1>This is place for timer</h1>
         </Col>                    
